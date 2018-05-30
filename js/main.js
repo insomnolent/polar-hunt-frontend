80b3ba2fd72787a87ad5f075a68d19bf4741b9c5
Justin Chang <hunteroflol1@gmail.com>
2018-05-30 10:12:42 -0700

    does hasura workpls

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+var token;
+var userId;
+
 	var authURL="https://auth.conventionalize82.hasura-app.io/"
 	var dataURL="https://data.conventionalize82.hasura-app.io/"
 	//on register click
@@ -8,13 +12,54 @@ $(document).ready(function(){
 		url: authURL='signup',
 		method:'post',
 		headers:{'Content-Type' : 'application/json'},
-		data.JSON.stringify({
-			"username": 'newusername',
-			"password": 'newpassword'
-		})
+		data: JSON.stringify({
+		"provider" : "username",
+		"data": {
+		"username" :$('#uname').val(), 
+		"password" :$('#pwd').val()
+		}
+	})
 
 	}).done(function(){
-		
+		//usr logged in here
+		alert('user logged in');
+	}).fail(function(data)){
+		console.error(data);
+		alert("fail :" +JSON.parse(data.responseText).message )
+	});
+//on click login 
+$(login).click(function(){
+//post request to login end pt
+
+#.ajax({
+	url: authUrl + 'login',
+	method: 'post',
+	headers: {'Content-Type': 'application/json'},
+	data: JSON.stringify({
+		"provider" : "username",
+		"data": {
+		"username" :$('#loginname').val(), 
+		"password" :$('#loginpassword').val()
+		}
 	})
+}).done(function(data){
+	token=data.auth_token;
+	userId=data.hasura_id;
+	userRoles=data.hasura_roles;
+	//redirect to app
+	window.location='/app';
+
+
+
+	//some expire time shit and cookies
+	var d= new Date();
+	d.setTime(d.getTime() + (1*24*60*60*1000));
+	var expires= "expires" + d.toUTCString();
+	document.cookie="cookie_name"+ "=" +token +" ;"+ expires + ";path=/";
+
 })
 });
+
+})
+
+}); 
