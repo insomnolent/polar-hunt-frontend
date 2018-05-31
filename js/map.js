de0f3c4f6d2b939d35cca76b4a61d166d65f6786
Amy Tu <tu.amy.a@gmail.com>
2018-05-30 21:14:00 -0700

    Realized how bad my attempts at creating a logo were, added markers to map

--- js/map.js
+++ js/map.js
@@ -3,10 +3,33 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
 
 var map;
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: {lat: 34.069, lng: -118.447},
+  });
+
+  addMarker({lat: 34.0709, lng: -118.444982}, map, 'Bruin Bear');
+  addMarker({lat: 34.071922, lng: -118.4496}, map, 'Bruin Plate');
+}
+
+function addMarker(location, map, contentString) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map,
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
   });
 }
