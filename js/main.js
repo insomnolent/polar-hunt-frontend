045ba84a12577bb35fd7ebd504788abb5b4342d8
Justin Chang <hunteroflol1@gmail.com>
2018-05-31 13:22:25 -0700

    added a new javascript for when your logged in, you can access your attributes and logout button. (need to create that logout button)

--- js/main.js
+++ js/main.js
@@ -22,13 +22,13 @@ $.ajax({
 	type: "POST",
 	dataType: "json"
 }).done(function(json) {
-	alert('user crated');
+	alert('user created');
 	// Handle Response
 	// To save the auth token received to offline storage
 	 var authToken = result.auth_token
 	 window.localStorage.setItem('HASURA_AUTH_TOKEN', authToken);
 }).fail(function(xhr, status, errorThrown) {
-
+	alert('failed to create an account, please try again')
 	console.log("Error: " + errorThrown);
 	console.log("Status: " + status);
 	console.dir(xhr);
@@ -82,6 +82,7 @@ $.ajax({
 	type: "POST",
 	dataType: "json"
 }).done(function(data){
+
 	token=data.auth_token;
 	userId=data.hasura_id;
 	userRoles=data.hasura_roles;
@@ -96,7 +97,7 @@ $.ajax({
 	//document.cookie="cookie_name"+ "=" +token +" ;"+ expires + ";path=/";
 
 }).fail(function(xhr, status, errorThrown) {
-
+		alert('failed to login, please try again!')
 	console.log("Error: " + errorThrown);
 	console.log("Status: " + status);
 	console.dir(xhr);
@@ -105,6 +106,52 @@ $.ajax({
 });
 
 });
+
+// If you have the auth token saved in offline storage, wanat to get my info this is how to do it
+/*
+ var authToken = window.localStorage.getItem('HASURA_AUTH_TOKEN');
+ headers = { "Authorization" : "Bearer " + authToken }
+$.ajax({
+	url: "https://auth.conventionalize82.hasura-app.io/v1/user/info",
+	contentType: "application/json",
+	headers: {
+      "Authorization": "Bearer "+authToken
+	},
+	data: JSON.stringify(null),
+	type: "GET",
+	dataType: "json"
+}).done(function(json) {
+	// Handle Response
+}).fail(function(xhr, status, errorThrown) {
+	console.log("Error: " + errorThrown);
+	console.log("Status: " + status);
+	console.dir(xhr);
+});
+
+
+
+//I WANT TO LOGOUT heres how to do it
+// If you have the auth token saved in offline storage
+ var authToken = window.localStorage.getItem('HASURA_AUTH_TOKEN');
+ headers = { "Authorization" : "Bearer " + authToken }
+$.ajax({
+	url: "https://auth.conventionalize82.hasura-app.io/v1/user/logout",
+	contentType: "application/json",
+	headers: {
+      "Authorization": "Bearer " +authToken
+	},
+	data: JSON.stringify(null),
+	type: "POST",
+	dataType: "json"
+}).done(function(json) {
+	// Handle Response
+}).fail(function(xhr, status, errorThrown) {
+	console.log("Error: " + errorThrown);
+	console.log("Status: " + status);
+	console.dir(xhr);
+});
+*/
+
 
 })
 
