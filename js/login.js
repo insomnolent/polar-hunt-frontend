f143ed19c99b462a0a3a2eb81998542076d10026
Amy Tu <tu.amy.a@gmail.com>
2018-05-31 22:19:19 -0700

    Hide/Show pages depending on whether user is logged in

--- js/login.js
+++ js/login.js
@@ -1,3 +1,75 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+$(document).ready(function(){
+
+	var token;
+	var userId;
+
+	var authURL="https://auth.conventionalize82.hasura-app.io/v1/"
+	var dataURL="https://data.conventionalize82.hasura-app.io/v1/"
+
+	toggleDisplay(fuckthis);
+
+	$(create).click(function() {
+  	$.ajax({
+    	url: "https://auth.conventionalize82.hasura-app.io/v1/signup",
+    	contentType: "application/json",
+    	data: JSON.stringify({
+      	"provider": "username",
+      	"data": {
+        	"username": $("#uname").val(),
+        	"password": $("#pwd").val()
+      	}
+    	}),
+    	type: "POST",
+    	dataType: "json"
+  	}).done(function(json) {
+    	alert('user created');
+    	// Handle Response
+    	// To save the auth token received to offline storage
+    	var authToken = result.auth_token
+    	window.localStorage.setItem('HASURA_AUTH_TOKEN', authToken);
+  	}).fail(function(json, xhr, status, errorThrown) {
+    	alert('failed to create an account, please try again: ' + JSON.parse(json.responseText).message);
+    	console.log("Error: " + errorThrown);
+    	console.log("Status: " + status);
+    	console.dir(xhr);
+  	});
+	}); //creating user
+
+	//on click login
+	$(login).click(function() {
+  	//post request to login end pt
+  	$.ajax({
+    	url: "https://auth.conventionalize82.hasura-app.io/v1/login",
+    	contentType: "application/json",
+    	data: JSON.stringify({
+      	"provider": "username",
+      	"data": {
+        	"username": $("#loginname").val(),
+        	"password": $("#loginpassword").val()
+      	}
+    	}),
+    	type: "POST",
+    	dataType: "json"
+  	}).done(function(data) {
+			window.localStorage.setItem('isLoggedIn', true);
+			toggleDisplay(fuckthis);
+    	token = data.auth_token;
+    	userId = data.hasura_id;
+    	userRoles = data.hasura_roles;
+    	//redirect to app
+    	alert('User logged in');
+    	window.location = 'capture.html';
+  	}).fail(function(data, xhr, status, errorThrown) {
+    	alert('failed to login, please try again! The error was ' + JSON.parse(data.responseText).message);
+    	console.log("Error: " + errorThrown);
+    	console.log("Status: " + status);
+    	console.dir(xhr);
+			window.localStorage.setItem('isLoggedIn', false);
+			toggleDisplay(fuckthis);
+  	});
+	}); //login
+}) //document.ready
--- js/toggle-display.js
+++ js/toggle-display.js
@@ -1,15 +1,23 @@
-var login = window.localStorage.getItem('isLoggedIn');
+function toggleDisplay(callback){
+   var login = window.localStorage.getItem('isLoggedIn');
+   console.log(login);
+   callback(login);
+};
 
-$(document).ready ( function(){
-   if (login) {
-     console.log("In thingy");
-   //now show all the previously hidden pages
-   document.getElementById("capture").style.display = "initial";
-   document.getElementById("gallery").style.display = "initial";
-   document.getElementById("map").style.display = "initial";
-   document.getElementById("logout").style.display = "initial";
-   }
-   else {
-     console.log("notloggedin");
-   }
-})
+function fuckthis(login) {
+  var y = document.getElementsByClassName("logged");
+  console.log(y);
+  if (login === "true") {
+    console.log(login);
+    console.log("logged in");
+    for (i = 0; i < y.length; i++) {
+      y[i].style.display = "initial";
+    }
+  } else {
+    console.log(login);
+    console.log("not logged in");
+    for (i = 0; i < y.length; i++) {
+      y[i].style.display = "none";
+    }
+  }
+}
